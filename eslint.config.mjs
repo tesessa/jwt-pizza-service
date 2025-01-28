8e9f9b98f8d745613b02174cdc089f829b0cfc5a
tesessa <tessaeliseandersen@yahoo.com>
2025-01-27 23:24:53 -0700

    updating eslint to recognize jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  {languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
 ];
