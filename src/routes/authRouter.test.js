d7c78118fc6bf193da48f63fb6b700b5818ec6a0
tesessa <tessaeliseandersen@yahoo.com>
2025-03-01 23:40:49 -0700

    dockerFile

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -94,6 +94,7 @@ const { Role, DB } = require('../database/database.js');
 async function createAdminUser() {
   let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
   user.name = randomName();
+  //user.name = "name"
   user.email = user.name + '@admin.com';
 
   user = await DB.addUser(user);
